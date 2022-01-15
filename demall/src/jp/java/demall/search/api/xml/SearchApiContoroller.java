7ad0af019a5b5e3f4da593168ea82abda3ecb1ac
ict21Otani <ict21otani@outlook.jp>
2022-01-15 20:52:51 +0900

    api機能にjsonの出力を追加

--- demall/src/jp/java/demall/search/api/xml/SearchApiContoroller.java
+++ demall/src/jp/java/demall/search/api/xml/SearchApiContoroller.java
@@ -14,15 +14,20 @@ import javax.servlet.http.HttpServletResponse;
 import jp.java.demall.common.constant.ConstFilePath;
 import jp.java.demall.search.ItemSearchController;
 
+/***
+ * api機能のコントローラー
+ * @author shisa
+ *
+ */
 @WebServlet("/search-api")
-public class SearchApiXmlContoroller extends ItemSearchController {
+public class SearchApiContoroller extends ItemSearchController {
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		//URL直たたきでくる前提
 
-		SearchApiXmlService service = new SearchApiXmlService();
-		List<String> list=new ArrayList();
+		SearchApiService service = new SearchApiService();
+		List<String> list = new ArrayList<String>();
 		try {
 			//XMLの内容を取得
 			list = service.execute(req);
@@ -32,26 +37,34 @@ public class SearchApiXmlContoroller extends ItemSearchController {
 			resp.sendRedirect(ConstFilePath.ERROR_HTML);
 			return;
 		}
+		//正常に帰ってきたときだけ処理する。
+		if (null != list) {
+			//XMLの時だけ設定する。
+			if (req.getParameter("resp-type").equals("xml")) {
+				//出力設定をXML
+				resp.setContentType("text/xml;charset=UTF-8");
+			}else if(req.getParameter("resp-type").equals("json")){
+				resp.setContentType("application/json");
+				resp.setCharacterEncoding("UTF-8");
+			}
+			PrintWriter out = resp.getWriter();
+			//XMLを一行ずつ出力する
+			for (String apiStr : list) {
+				out.println(apiStr);
+			}
+		}else {
+			//エラーの時
+			resp.sendRedirect(ConstFilePath.ILLEGALE_ACCESS);
+			return;
+		}
 
-		 //出力設定をXML
-		 resp.setContentType("text/xml;charset=UTF-8");
-
-		 PrintWriter out = resp.getWriter();
-		 //XMLを一行ずつ出力する
-		 for( String xmlStr:list) {
-			 out.println(xmlStr);
-		 }
-
-
-	
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		//TODO ポストで飛んでくる場合はフォーム画面がいる。
 		//POSTのリクエストを受け取ってdoGetに処理を委譲
-		doGet(req,resp);
+		doGet(req, resp);
 	}
-
 
 }
---
+++ demall/src/jp/java/demall/search/api/xml/SearchApiService.java
@@ -0,0 +1,122 @@
+package jp.java.demall.search.api.xml;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import jp.java.demall.dao.ItemsDAO;
+import jp.java.demall.search.Items;
+
+/**
+ * API用のサーブレットレスポンスはXML
+ * @author ta2ro
+ *
+ */
+public class SearchApiService {
+	/***
+	 *
+	 * Json,XMLを生成して返す。（文字列のリスト）
+	 *
+	 * @param request
+	 * @return
+	 * @throws SQLException
+	 */
+	public List<String> execute(HttpServletRequest request) throws SQLException {
+
+		//データ取得
+		ItemsDAO dao = new ItemsDAO();
+		List<Items> itemList = new ArrayList<Items>();
+		try {
+
+			itemList = dao.findByKeywordAndCategory(request.getParameter("keyword"),
+					Integer.parseInt(request.getParameter("category")));
+		} catch (SQLException e) {
+			throw e;
+		}
+		String type = request.getParameter("resp-type");
+
+		//戻り値用用リスト
+		List<String> returnList = new ArrayList<String>();
+		if (null != type && false==type.equals("")) {
+			//リクエストがnullでなくて文字でない時
+			//XML指定
+			if (type.equals("xml")) {
+				//XMLの処理
+
+				List<String> xmlList = new ArrayList<String>();
+				//データをXML形式に直す インデントはブラウザが勝手にやってくださるのでここで入れたりしない。
+				//最初の一行
+				xmlList.add("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+
+				//二行目以降
+				xmlList.add("<Items>");
+
+				//繰り返し
+				for (Items item : itemList) {
+					xmlList.add("<Item>");
+					//ITEM名
+					xmlList.add("<name>" + item.getName() + "</name>");
+					//価格
+					xmlList.add("<price>" + item.getPrice() + "</price>");
+					//カテゴリ
+					xmlList.add("<category>" + item.getCategoryId() + "</category>");
+
+					//閉じたぐ
+					xmlList.add("</Item>");
+					//繰り返し
+				}
+				//閉じたぐ
+				xmlList.add("</Items>");
+				//xmlList.add("</xml>");//一番親の閉じたぐ XMLに閉じタグいらない
+
+				//戻り値セット
+				returnList=xmlList;
+			}else if(type.equals("json")){
+				List<String> jsonList = new ArrayList<String>();
+				int cnt=0;//回数カウント用
+				//最初の行
+				jsonList.add("{ \"Item\":[");
+
+				//繰り返し部分
+				for (Items item : itemList) {
+					cnt++;
+					//配列始まりタグ
+					jsonList.add("{");
+					//アイテム名
+					jsonList.add("\"name\": \""+item.getName()+"\",");
+					//プライス
+					jsonList.add("\"price\":" +item.getPrice()+",");
+					//カテゴリ
+					jsonList.add("\"category\":"+item.getCategoryId());
+
+					//最終行以外
+					if(cnt<itemList.size()) {
+					//,をいれる
+						jsonList.add("},");
+
+					}else {
+						//最終行は,なし。
+						jsonList.add("}");
+					}
+				}
+				//最後の行
+				jsonList.add("]}");
+
+				//戻り値セット
+				returnList=jsonList;
+			}
+
+
+		} else {
+			//nullを返して　コントローラーでエラー処理
+			return null;
+
+		}
+
+		//Jsonの処理
+
+		return returnList;
+	}
+}
